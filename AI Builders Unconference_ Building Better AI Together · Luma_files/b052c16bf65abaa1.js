0ae2c954a25de16357a74a239cb37ba8c2e65242
dawg <3172733+le-dawg@users.noreply.github.com>
2026-02-02 10:39:10 +0100

    Delete AI Builders Unconference_ Building Better AI Together · Luma_files directory

--- AI Builders Unconference_ Building Better AI Together · Luma_files/b052c16bf65abaa1.js
+++
@@ -1,5 +0,0 @@
-;!function(){try { var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof global?global:"undefined"!=typeof window?window:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&((e._debugIds|| (e._debugIds={}))[n]="e15d9f41-8d94-74cb-75a9-e7e36facca05")}catch(e){}}();
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,72326,e=>{"use strict";var t=e.i(498076),n=e.i(581455),i=e.i(348688);let r=async e=>{let{initialToken:r}=e;return new t.Centrifuge(n.config.CENTRIFUGO_URL,{token:r,getToken:async()=>{let{user:e}=await i.APIClient.get("/user");return e.centrifugo_user_token}})};e.s(["createCentrifugeConnection",0,r])}]);
-
-//# debugId=e15d9f41-8d94-74cb-75a9-e7e36facca05
-//# sourceMappingURL=de9032b7e8f77e0a.js.map
